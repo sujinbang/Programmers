3f01248d6d6805d0994059961dbf073fdd41d96d
SJBANG <104615408+sujinbang@users.noreply.github.com>
2022-10-26 22:42:10 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,15 @@
+# -- 코드를 입력하세요
+# SELECT ANIMAL_ID, 
+#        NAME
+#        DECODE(SEX_UPON_INTAKE, '%Neutered%', 'O')
+       
+#   FROM ANIMAL_INS
+
+
+SELECT ANIMAL_ID, NAME,
+  CASE WHEN SEX_UPON_INTAKE LIKE '%Neutered%' THEN 'O'
+  WHEN SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O' 
+  ELSE 'X'
+  END AS '중성화'
+  FROM ANIMAL_INS
+ ORDER BY ANIMAL_ID
