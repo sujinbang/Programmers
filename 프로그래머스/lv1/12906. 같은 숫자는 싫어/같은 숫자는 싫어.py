9494a0985e67eff8477eebf179e442899dda2a0d
SJBANG <104615408+sujinbang@users.noreply.github.com>
2022-11-02 15:56:15 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 157.41 ms, Memory: 28 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    stack = []
+    result = [arr[0]]
+    for num in arr:
+        if stack:
+            curr = stack.pop()
+            if curr != num:
+                result.append(num)
+
+        stack.append(num)
+    return result
